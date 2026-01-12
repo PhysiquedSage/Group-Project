cac81d89f202b8363af8d75626cf9f81fba56b94
Paulogit24 <40240257@esmad.ipp.pt>
2026-01-12 17:29:01 +0000

    update 2

--- js/data.js
+++ js/data.js
@@ -1,6 +1,13 @@
-var users = [
-    { id: 1, username: "Alice", password: "alice123" },
-    { id: 2, username: "Bob", password: "bob123" },
-    { id: 3, username: "Charlie", password: "charlie123" },
-    { id: 4, username: "Diana", password: "diana123" }
-];
+const data = {
+    users: [
+        { id: 1, username: "Alice", password: "alice123" },
+        { id: 2, username: "Bob", password: "bob123" },
+        { id: 3, username: "Charlie", password: "charlie123" },
+        { id: 4, username: "Diana", password: "diana123" }
+    ],
+    movies: [
+        { id: 1, title: "Into the Spider-Verse", genre: "Action", img: "/img/Into the spider.jpg",},
+    ]
+};
+
+export default data;
--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-import "/js/data.js";
-
-console.log("User Data:", users);
+import data from "/js/data.js";
+localStorage.setItem("userData", JSON.stringify(data.users));
+console.log("User Data:", JSON.parse(localStorage.getItem("userData")));
