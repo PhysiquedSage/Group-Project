afff4227041292778c80ca62a24a4f370e38bd5e
Paulogit24 <40240257@esmad.ipp.pt>
2026-01-16 11:25:21 +0000

    function login

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 import data from "/js/data.js";
 localStorage.setItem("userData", JSON.stringify(data.users));
 console.log("User Data:", JSON.parse(localStorage.getItem("userData")));
+localStorage.setItem("movieData", JSON.stringify(data.movies));
+
+console.log("Movie Data:", JSON.parse(localStorage.getItem("movieData")));
+localStorage.setItem("currentUser", JSON.stringify({}));
+
+function login(username,password){
+    error = true
+    users = JSON.parse(localStorage.getItem("userData"))
+    for (let i = 0; i < users.length;i++ ){
+        if (username == users[i].username && password == users[i].password){
+            error = false
+            localStorage.setItem("currentUser", JSON.stringify(users[i]))
+            console.log("Login successful")
+            break
+        }
+    }
+    if (error){
+        console.log("Login failed")
+    }
+}
+
+function logout(){
+    localStorage.setItem("currentUser", JSON.stringify({}))
+    console.log("Logged out")
+};
