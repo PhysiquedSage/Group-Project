cd3d626ff03d15285a7f1ac969875a7df36e27c6
HenriqueOliveira08 <henriquesadeoliveira@gmail.com>
2026-01-18 11:05:30 +0000

    cataloged the movies in data.js

--- js/data.js
+++ js/data.js
@@ -8,6 +8,15 @@ const data = {
     ],
     movies: [
         {title: "Into the Spider-Verse", genre: ["Action"], img: "/img/Into the spider.jpg"},
+        {title: "The Interview", genre: ["Comedy"], img: "/img/the-interview.jpg"},
+        {title: "Pulp Fiction", genre: ["Crime"], img: "/img/pulp-fiction.jpg"},
+        {title: "John Wick", genre: ["Action"], img: "/img/John Wick.jpg"},
+        {title: "Hot Fuzz", genre: ["Comedy"], img: "/img/hot-fuzz.jpg"},
+        {title: "Avatar: Fire and Ash", genre: ["Action"], img: "/img/fire-and-ash.jpg"},
+        {title: "Dungeons & Dragons", genre: ["Fantasy"], img: "/img/dnd.jpg"},
+        {title: "Deadpool", genre: ["Action"], img: "/img/deadpool.jpg"},
+        {title: "Central Intelligence", genre: ["Comedy"], img: "/img/CI.jpg"},
+        {title: "1917", genre: ["War"], img: "/img/1917.jpg"}
     ]
 };
 
