f24a7b47adc5de884308be7e35b57834bbc0ee2b
Paulogit24 <40240257@esmad.ipp.pt>
2026-01-19 16:08:35 +0000

    small data update

--- js/data.js
+++ js/data.js
@@ -7,16 +7,16 @@ const data = {
         {username: "jeff", password: "jeff" }
     ],
     movies: [
-        {title: "Into the Spider-Verse", genre: ["Action"], img: "/img/Into the spider.jpg"},
-        {title: "The Interview", genre: ["Comedy"], img: "/img/the-interview.jpg"},
-        {title: "Pulp Fiction", genre: ["Crime"], img: "/img/pulp-fiction.jpg"},
-        {title: "John Wick", genre: ["Action"], img: "/img/John Wick.jpg"},
-        {title: "Hot Fuzz", genre: ["Comedy"], img: "/img/hot-fuzz.jpg"},
-        {title: "Avatar: Fire and Ash", genre: ["Action"], img: "/img/fire-and-ash.jpg"},
-        {title: "Dungeons & Dragons", genre: ["Fantasy"], img: "/img/dnd.jpg"},
-        {title: "Deadpool", genre: ["Action"], img: "/img/deadpool.jpg"},
-        {title: "Central Intelligence", genre: ["Comedy"], img: "/img/CI.jpg"},
-        {title: "1917", genre: ["War"], img: "/img/1917.jpg"}
+        {title: "Into the Spider-Verse", genre: ["Action", "Animation", "Comedy"], img: "/img/Into the spider.jpg"},
+        {title: "The Interview", genre: ["Comedy", "Drama", "Thriller"], img: "/img/the-interview.jpg"},
+        {title: "Pulp Fiction", genre: ["Crime", "Drama", "Thriller"], img: "/img/pulp-fiction.jpg"},
+        {title: "John Wick", genre: ["Action", "Crime", "Thriller"], img: "/img/John Wick.jpg"},
+        {title: "Hot Fuzz", genre: ["Comedy", "Action", "Mystery"], img: "/img/hot-fuzz.jpg"},
+        {title: "Avatar: Fire and Ash", genre: ["Action", "Adventure", "Sci-Fi"], img: "/img/fire-and-ash.jpg"},
+        {title: "Dungeons & Dragons", genre: ["Fantasy", "Adventure", "Action"], img: "/img/dnd.jpg"},
+        {title: "Deadpool", genre: ["Action", "Comedy", "Superhero"], img: "/img/deadpool.jpg"},
+        {title: "Central Intelligence", genre: ["Comedy", "Action", "Adventure"], img: "/img/CI.jpg"},
+        {title: "1917", genre: ["War", "Drama", "History"], img: "/img/1917.jpg"}
     ]
 };
 
